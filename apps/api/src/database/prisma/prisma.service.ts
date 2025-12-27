213703209a0fb1cd612fd8f69fa7d5eebdf322c5
Juan Silva <52308574+JuanS1lva@users.noreply.github.com>
2025-12-26 23:17:39 -0500

    Add Prisma integration

---
+++ apps/api/src/database/prisma/prisma.service.ts
@@ -0,0 +1,33 @@
+import { INestApplication, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: configService.getOrThrow<string>('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication): Promise<void> {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
